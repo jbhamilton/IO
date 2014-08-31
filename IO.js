c36287f0e5055cee76e092bc934d83d3d0354ae4
Bradley Hamilton <bradleygoskimmin@gmail.com>
2014-08-30 22:44:50 -0400

    init

---
+++ IO.js
@@ -0,0 +1,177 @@
+var IO = {};
+IO.patterns = {
+'alpha':'^[a-zA-Z\s\-]+$',
+'alpha_numeric':'^[a-zA-Z\s\-0-9]+$',
+'integer':'^[\-0-9]+$',
+'numeric':'^[\-0-9\.]+$',
+'all':'[.]*'
+};
+
+IO.cache = {};
+IO.errors = {};
+
+IO.set = function(e){
+    var key = IO.tojQuery(e).selector;
+    if(IO.cache[key]){
+        return;
+    }//if
+    IO.cache[key] = IO.get(e);
+}//listen
+
+IO.subscribe = function(e,callback){
+    var $e = IO.tojQuery(e),
+        form = IO.get($e);
+    for(attr in form){
+        $e.find('[name="'+attr+'"]').change(function(){
+            var o = {};
+            if($(this).attr('type')=='checkbox'){
+                o[$(this).attr('name')] = ($(this).prop('checked')) ? 'on' : 'off';
+            }//if
+            else {
+                o[$(this).attr('name')] = $(this).val();
+            }//el
+            callback(o); 
+        });
+    }//for
+
+}//listen
+
+IO.changes = function(e){
+
+    IO.set(e);
+
+    var key = IO.tojQuery(e).selector,
+        cache = IO.cache[key],
+        now = IO.get(e),
+        changed = {},
+        changes = false;
+
+    for(var attr in now){
+        if(now[attr] != cache[attr]){
+            changes = true;
+            IO.cache[key][attr] = now[attr];
+            changed[attr] = now[attr];
+        }//if
+    }//for
+
+    if(changes){
+        return changed;
+    }//if
+
+    return false;
+
+}//changes
+
+IO.get = function(e){
+
+    var $e = IO.tojQuery(e),
+        errorCheck = ($e.attr('ioerrors')!==undefined) ? true : false;
+
+    if( $e.parent().prop('tagName') != 'FORM' &&
+        $e.prop('tagName') != 'FORM'
+    ){
+        $e.wrap('<form class="io-form"></form>');
+        $e = $(e).parent();
+    }//if
+
+    if($e.prop('tagName') != 'FORM'){
+        $e = $e.parent();
+    }//if
+
+    var i = $e.serialize(),
+        o = {},
+        s = i.split('&'),
+        e = false;
+
+    for(var iter = 0; iter < s.length; iter++){
+        var ss = s[iter].split('='),
+            key = ss[0],
+            value = ss[1];
+        o[key] = value;
+        
+        var $t = $e.find('[name="'+key+'"]');
+        if(errorCheck && !IO.match($t)){
+            e = true; 
+            var $n = $t.next();
+            if(!$n.hasClass('io-error')){
+                return;
+            }//if
+            $n.css('display','block');
+        }//if
+
+    }//for
+
+    if(errorCheck){
+        if(e){
+            $e.addClass('io-form-errors');
+        }//if
+        else {
+            $e.removeClass('io-form-errors');
+        }//el
+    }//if
+
+    $e.find('[type="checkbox"]').each(function(){
+        if($(this).prop('checked')){
+            return;
+        }//if
+        o[$(this).attr('name')] = 'off';
+    });
+
+    return o;
+
+}//get
+
+IO.match = function($i){
+    var pattern = $i.attr('match');
+    if(!pattern){
+        return true;
+    }//if
+
+    if(IO.patterns[pattern]){
+       pattern = IO.patterns[pattern]; 
+    }//if
+
+    pattern = new RegExp(pattern);
+    var value = $i.val();
+
+    if(value.match(pattern)){
+        return true;
+    }//if
+
+    return false;
+}//match
+
+IO.hasError = function(e){
+    IO.get(e);
+    if(IO.tojQuery(e).parent().hasClass('io-form-errors') ||
+       IO.tojQuery(e).hasClass('io-form-errors')
+    ){
+        return true;
+    }//if
+    return false;
+}//hasError
+
+IO.tojQuery = function(e){
+    if(e instanceof jQuery){
+        return e;
+    }//if
+    return $(e); 
+}//tojQuery
+
+$(function(){
+    $('[ioerrors]').find('[match]').each(function(){
+        $(this).change(function(){
+            var $e = $(this).next();
+            if(!$e.hasClass('io-error')){
+                return;
+            }//if
+            if(IO.match($(this))){
+                $e.css('display','none');
+            }//if
+            else {
+                $e.css('display','block');
+            }//el
+        });
+    });
+    $('[ioerrors]').find('.io-error').css('display','none');
+});
---
+++ README.md
@@ -0,0 +1,18 @@
+<h1>IO - ultimate javscript form data control and validation</h1>
+
+<p>Web forms make the web an interactive place, and drive business centric applications. Giving your users and
+clients an awesome and easy web form expierence shouldn't be difficult, and it doesn't have to be. Using IO you
+can easily build efficient self updating forms (think google docs) or simply save resources by limiting data
+communication to strictly updated data. Easily provide matching criteria for inputs and auto-toggle error messages
+and the ability to ask IO if the form has an error.
+You can also prevent major hair loss by having it in your toolkit the next
+time your boss asks you to build up a new screen with lots of data pieces.</p>
+
+<h2>Get me the data!</h2>
+<p>Get a containers data:</p>
+
+```javascript
+var data = IO.get('.data-wrapper');
+```
+
+
